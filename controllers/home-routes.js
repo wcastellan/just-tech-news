2bf9b77d671458b8222c42889a693f27bc28db3b
Wyatt C <wcastellan@yahoo.com>
2022-01-11 11:48:11 -0500

    add #if helpers to the code to help clean it up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
         // pass a single post object into the homepage template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
@@ -85,7 +85,7 @@ router.get('/post/:id', (req, res) => {
     const post = dbPostData.get({ plain: true });
 
     // pass data to template
-    res.render('single-post', { post });
+    res.render('single-post', { post, loggedIn: req.session.loggedIn });
   })
   .catch(err => {
     console.log(err);
